2d2ab9c2c8a87d544a20dd79273df879fc175605
Clint Favor <clint.favor@mllrdev.com>
2018-05-11 17:34:39 +0800

    Initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZpuGJ1xoijxOa1e7KUoUxuzcxwTyjiTc",
+  authDomain: "wirebrain-bc710.firebaseapp.com",
+  databaseURL: "https://wirebrain-bc710.firebaseio.com",
+  storageBucket: "wirebrain-bc710.appspot.com",
+  messagingSenderId: "58054965950"
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule { }
---
+++ src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  menuData = [
+    {title: 'Our Menu', pic: 'assets/img/soup1.jpg', pushPage: 'MenuPage'},
+    {title: 'Account', pic: 'assets/img/coffee-people3.jpg', pushPage: 'AccountPage'},
+    {title: 'About Us', pic: 'assets/img/coffee6.jpg', pushPage: 'AboutPage'},
+    {title: 'Locations', pic: 'assets/img/cafe2.jpg', pushPage: 'LocationsPage'}
+  ];
+
+  logPage: any
+
+  constructor(public navCtrl: NavController) {
+    this.logPage = 'LoginPage';
+  }
+
+}
